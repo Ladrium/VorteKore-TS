1ef8f3472271d13cedd7e4f8d1b10bc03eb008ce
johna3212 <52121529+johna3212@users.noreply.github.com>
2019-11-18 16:08:36 +0530

    Delete config.js

--- dist/config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    token: "NjM0NzY2OTYyMzc4OTMyMjI0.XbB_6g.gNu1etkrLLVd2Vewdb9oNwuEIi0",
-    uri: "mongodb+srv://chaos:iscool@vortekore-hu9xg.mongodb.net/TS?retryWrites=true&w=majority"
-};

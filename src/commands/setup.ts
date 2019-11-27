ce5a1751f1086b9e0b7b7a23219dd25ad5b13a56
Johna3212 <hackgamersno1@gmail.com>
2019-11-27 21:06:59 +0530

    Update setup.ts

--- src/commands/setup.ts
+++ src/commands/setup.ts
@@ -2,6 +2,8 @@ import { Command } from "../structures/Command";
 import { VorteClient } from "../structures/VorteClient";
 import { Message } from "discord.js";
 import { VorteGuild } from "../structures/VorteGuild";
+import { checkPermissions } from "../util";
+import VorteEmbed from "../structures/VorteEmbed";
 
 export class Cmd extends Command {
   constructor(bot: VorteClient) {
@@ -11,7 +13,16 @@ export class Cmd extends Command {
       cooldown: 200
     })
   }
+  
   run(message: Message, args: string[], guild: VorteGuild) {
-
+    if (args[0] === "prefix") {
+      if (!checkPermissions(message.member!, "ADMINISTRATOR")) return message.channel.send(`Missing Permissions for using this command.`); 
+      if (!args[1]) {
+        message.channel.send(new VorteEmbed(message).baseEmbed().setTitle(`Current prefix is: \`${guild.prefix}\``))
+      } else {
+        guild.setPrefix(args[1]);
+        message.channel.send(`Successfully changed the prefix to ${args[1]}`);
+      };
+    }
   }
 }
